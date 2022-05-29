f85d50a25896a5007936b5315080fbebfbfd2f7c
hulknosmash <105942664+hulknosmash@users.noreply.github.com>
2022-05-29 15:16:37 -0400

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -1,4 +1,4 @@
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo -y &
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg -y &
 sudo dnf update  && sudo dnf upgrade -y &
-sudo dnf install lightdm qutebrowser alacritty bspwm sublime-text
+sudo dnf install lightdm qutebrowser alacritty bspwm sublime-text -y
