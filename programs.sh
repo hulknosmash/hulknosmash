0a46e7e15dff82a55e3f50356de8426c8afbced2
hulknosmash <105942664+hulknosmash@users.noreply.github.com>
2022-05-29 15:42:59 -0400

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -1,6 +1,6 @@
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo -y &
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg -y &
 sudo dnf update  && sudo dnf upgrade -y &
-sudo dnf install lightdm qutebrowser tty-clock gimp alacritty bspwm sublime-text -y &
+sudo dnf install lightdm qutebrowser picom tty-clock gimp alacritty bspwm sublime-text -y &
 sudo systemctl enable lightdm & 
-Sudo systemctl set-default graphical.target
+sudo systemctl set-default graphical.target
